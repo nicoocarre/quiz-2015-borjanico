ae76750c2ea57ce8f6ea6d448acd25cc95140af6
borjacore <borja_011sr@hotmail.com>
2015-05-14 13:02:44 +0200

    despliegue en heroku con autores

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 //app.use('/users', users);
+app.use('/authors', autores);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
