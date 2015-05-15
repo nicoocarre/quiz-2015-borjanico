26178404c671fdf9cda56b21c544b3663efc0147
borjacore <borja_011sr@hotmail.com>
2015-05-15 16:55:51 +0200

    version7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,31 +1,37 @@
 var models = require('../models/models.js');
 
 
+
+
+
+// GET /quizes/:id
+exports.show = function(req, res){
+  models.Quiz.find(req.params.quizId).then(function(quiz){
+    res.render('quizes/show', {quiz: quiz});
+  })
+};
+
+
+// GET /quizes/answer
+exports.answer = function(req, res){
+  models.Quiz.find(req.params.quizId).then(function(quiz){
+    if(req.query.respuesta === quiz.respuesta){
+      res.render('quizes/answer', {quiz: quiz, respuesta:'Correcto'});
+    }else{
+      res.render('quizes/answer', {quiz: quiz, respuesta:'Incorrecto'});
+    }
+  })
+};
+
+
+
+
+
 // GET /quizes
 exports.index = function(req, res) {
   models.Quiz.findAll().then(
     function(quizes) {
-      res.render('quizes/index.ejs', {quizes: quizes, errors: []});
+      res.render('quizes/index.ejs', {quizes: quizes});
     }
-  ).catch(function(error){next(error)});
-};
-
-// GET /quizes/:id
-exports.show = function(req, res) {
-  res.render('quizes/show', { quiz: req.quiz, errors: []});
-};            // req.quiz: instancia de quiz cargada con autoload
-
-// GET /quizes/:id/answer
-exports.answer = function(req, res) {
-  var resultado = 'Incorrecto';
-  if (req.query.respuesta === req.quiz.respuesta) {
-    resultado = 'Correcto';
-  }
-  res.render(
-    'quizes/answer', 
-    { quiz: req.quiz, 
-      respuesta: resultado, 
-      errors: []
-    }
-  );
+  )
 };
