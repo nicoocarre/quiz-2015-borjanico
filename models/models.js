4a19f455ef744f374e34746a664bdf1f2dd4df99
borjacore <borja_011sr@hotmail.com>
2015-05-15 15:40:42 +0200

    version3

--- models/models.js
+++ models/models.js
@@ -48,10 +48,8 @@ sequelize.sync().then(function() {
   Quiz.count().then(function (count){
     if(count === 0) {   // la tabla se inicializa solo si está vacía
       Quiz.create( 
-        [ {pregunta: 'Capital de Italia',   respuesta: 'Roma'},
-          {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
-        ]
-      ).then(function(){console.log('Base de datos inicializada')});
+         {pregunta: 'Capital de Italia',   respuesta: 'Roma'});
+      .then(function(){console.log('Base de datos inicializada')});
     };
   });
 });
