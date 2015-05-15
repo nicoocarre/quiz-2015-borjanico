c930bc57dff17a005336b2948e9b6c7050780378
borjacore <borja_011sr@hotmail.com>
2015-05-15 09:47:31 +0200

    prueba base de datos2

--- models/models.js
+++ models/models.js
@@ -47,7 +47,7 @@ sequelize.sync().then(function() {
   // then(..) ejecuta el manejador una vez creada la tabla
   Quiz.count().then(function (count){
     if(count === 0) {   // la tabla se inicializa solo si está vacía
-      Quiz.Create( 
+      Quiz.create( 
         [ {pregunta: 'Capital de Italia',   respuesta: 'Roma'},
           {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
         ]
