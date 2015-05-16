d3b4df1a6acf1965e00a6638fedba4edc27a362e
borjacore <borja_011sr@hotmail.com>
2015-05-16 10:55:50 +0200

    nueva3

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+
 app.use('/authors', autores);
 
 // catch 404 and forward to error handler
